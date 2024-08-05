4d43d6700814535d699634b1f5231a8ec3c0953e
LSB1021 <118779507+LSB1021@users.noreply.github.com>
2024-08-05 21:26:39 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+
+  //修改启动
+  server: {
+    host: '0.0.0.0', //这个用于启动
+    port: 8092, //指定启动端口
+    open: true, //启动后是否自动打开浏览器
+  }
+})
