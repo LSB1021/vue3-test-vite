f43a399cee0c32a68d8241a22c9aa6f419bd50d5
LSB1021 <118779507+LSB1021@users.noreply.github.com>
2024-08-06 14:40:28 +0800

    Add files via upload

---
+++ 07_src_computed计算属性/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Demo>
+    <template v-slot:qwe>
+      <span>lsb</span>
+    </template>
+    <template v-slot:asd>
+      <span>lsb</span>
+    </template>
+  </Demo>
+</template>
+
+<script>
+  import Demo from './components/Demo.vue';
+  export default {
+    name:'App',
+    components:{
+      Demo
+    },
+    setup(){
+      function showHelloMsg(value){
+        alert(`你好啊，你触发了hello事件，我收到的参数时：${value}`)
+      }
+
+      return{
+        showHelloMsg
+      }
+    }
+  }
+</script>
